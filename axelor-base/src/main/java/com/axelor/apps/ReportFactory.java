7ebaf58f8d2d626c51d3ad022f6c9837afff5940
Arthur Lefebvre <a.lefebvre@axelor.com>
2020-02-10 14:23:05 +0100

    break spotless

--- axelor-base/src/main/java/com/axelor/apps/ReportFactory.java
+++ axelor-base/src/main/java/com/axelor/apps/ReportFactory.java
@@ -25,10 +25,7 @@ public class ReportFactory {
 
   public static ReportSettings createReport(String rptdesign, String outputName) {
 
-    if (ReportSettings.useIntegratedEngine()) {
-
-      return new EmbeddedReportSettings(rptdesign, outputName);
-    } else {
+    if (ReportSettings.useIntegratedEngine()) { return new EmbeddedReportSettings(rptdesign, outputName); } else {
 
       return new ExternalReportSettings(rptdesign, outputName);
     }
